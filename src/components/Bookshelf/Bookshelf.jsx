fef1f7e3e0df6d4ee8cfc2f4bc309f64cf2d2c46
Adil Khan <adil_97@hotmail.it>
2026-01-29 22:40:21 +0000

    .

---
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react"
+
+const Bookshelf = () => {
+  const [books, setBooks] = useState([
+    { title: "Fourth Wing", author: "Rebecca Yarros" },
+    { title: "The Lion, the Witch and the Wardrobe", author: "C.S. Lewis" }
+  ])
+
+  const [newBook, setNewBook] = useState({
+    title: "",
+    author: ""
+  })
+
+  const handleInputChange = (event) => {
+    const name = event.target.name
+    const value = event.target.value
+
+    setNewBook({...newBook, [name]: value})
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    setBooks([...books, newBook])
+
+    setNewBook({
+      title: "",
+      author: ""
+    })
+  }
+
+  return (
+    <div className="bookshelfDiv">
+      <div className="formDiv">
+        <h3>Add a Book</h3>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="title">Title</label>
+          <input
+            id="title"
+            type="text"
+            name="title"
+            value={newBook.title}
+            onChange={handleInputChange}
+          />
+
+          <label htmlFor="author">Author</label>
+          <input
+            id="author"
+            type="text"
+            name="author"
+            value={newBook.author}
+            onChange={handleInputChange}
+          />
+
+          <button type="submit">Add Book</button>
+        </form>
+      </div>
+
+      <div className="bookCardsDiv">
+        {books.map((book, index) => (
+          <div key={index} className="bookCard">
+            <h4>{book.title}</h4>
+            <p>{book.author}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Bookshelf
